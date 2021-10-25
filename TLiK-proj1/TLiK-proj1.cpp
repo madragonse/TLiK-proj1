621f0833c1e7ce70b60755c438131a9a83b58f30
Maciej Stefaniak <48411094+madragonse@users.noreply.github.com>
2021-10-25 14:09:08 +0200

    propability, entropy

--- TLiK-proj1/TLiK-proj1.cpp
+++ TLiK-proj1/TLiK-proj1.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <math.h>
 
 int main()
 {
 	std::ifstream inputFile;
-	size_t size = 0; // here
+	size_t size = 0;
 
 	int charAmount[256] = { 0 };
+	float charPropability[256] = { 0 };
+	int charDataSize[256] = { 0 };
 
 	inputFile.open("ENG_1.txt", std::ios::in | std::ios::binary | std::ios::ate);
 	char* inp;
@@ -19,16 +22,58 @@ int main()
 	inputFile.read(inp, size);
 	inp[size] = '\0';
 
+	int wholeCount = 0;
 	//print data
 	for (size_t i = 0; i < strlen(inp); i++)
 	{
 		char t = inp[i];
 		charAmount[t]++;
+		wholeCount++;
 	}
 
+	for (int i = 0; i < 256; i++)
+	{
+		charPropability[i] = double(charAmount[i] / double(wholeCount));
+	}
+
+	for (int i = 0; i < 256; i++)
+	{
+		if (charPropability[i] == 0)
+		{
+			continue;
+		}
+		charDataSize[i] = (int)ceil(log2(1 / charPropability[i]));
+	}
+
+	printf("\nAll letters counts\n");
 	for (int i = 0; i < 256; i++) {
 		printf("Char: %d (%c) - %d\n", i, i, charAmount[i]);
 	}
+
+	printf("\nAll letters propability\n");
+	for (int i = 0; i < 256; i++) {
+		printf("Char: %d (%c) - %f\n", i, i, charPropability[i]);
+	}
+
+	printf("\nAll letters data size in bits\n");
+	for (int i = 0; i < 256; i++) {
+		printf("Char: %d (%c) - %d\n", i, i, charDataSize[i]);
+	}
+
+	float messageEntropy = 0;
+
+	for (int i = 0; i < 256; i++)
+	{
+		if (charPropability[i] > 0)
+		{
+			messageEntropy += charPropability[i] * charDataSize[i];
+		}
+		
+	}
+
+
+	printf("All letters count: %d\n", wholeCount);
+	printf("Message entopy: %f\n", messageEntropy);
 
 	getchar();
 	return 0;
